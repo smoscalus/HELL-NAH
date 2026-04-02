9655de5945d7f34876b49968b45cf7fd0ed243bc
sas <smoscalus@gmail.com>
2026-04-02 11:59:24 +0300

    Create set and get methods

--- include/hellnah/Core/Id.h
+++ include/hellnah/Core/Id.h
@@ -10,7 +10,18 @@ namespace Core
         uint64_t id = 0;
 
     public:
-        void set_id(uint64_t id);
-        uint64_t get_id();
+        void set_id(uint64_t id)
+        {
+            if(id < 0)
+            {  
+                throw std::runtime_error("file is broke");          
+            }
+
+            this->id = id;
+        }
+        uint64_t get_id()
+        {
+            return this->id;
+        }
     };
 }
