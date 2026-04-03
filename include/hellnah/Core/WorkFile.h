5c9957cf690ec235d3d484bf8f84dce6db8b0b26
sas <smoscalus@gmail.com>
2026-04-03 09:59:24 +0300

    Update WorkFile

--- include/hellnah/Core/WorkFile.h
+++ include/hellnah/Core/WorkFile.h
@@ -5,10 +5,26 @@ namespace Core
     struct WorkFile
     {
     private:
-        char Value[256];
+        char path[256];
+        size_t size;
+
+        void create_file(const char *path);
+        void read_file(const char *path);
+        int is_exists_file(const char *path);
 
     public:
         WorkFile(const char *path);
-        const char *getValue() const { return Value; }
+        void set_size(size_t size)
+        {
+            this->size = size;
+        }
+        size_t get_size() const
+        {
+            return this->size;
+        }
+        const char *get_path() const
+        {
+            return this->path;
+        }
     };
 }
--- src/Core/WorkFile.cpp
+++ src/Core/WorkFile.cpp
@@ -2,18 +2,60 @@
 #include "../../include/hellnah/Core/DbHeader.h"
 #include "../../include/hellnah/Storage/FileStorage.h"
 
+#include <iostream>
+#include <fstream>
 #include <cstring>
+#include <cstddef>
 
-Core::WorkFile::WorkFile(const char *path)
+namespace Core
 {
-    if (!Storage::FileStorage::is_exists_file(path))
+    int WorkFile::is_exists_file(const char *path)
     {
-        Storage::FileStorage::create_file(path);
-    }
-    else
-    {
-        Storage::FileStorage::read_file(path);
+        std::fstream file(path, std::ios::in | std::ios::binary);
+        if (!file.fail())
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
     }
 
-    strcpy(Value, path);
+    void WorkFile::create_file(const char *path)
+    {
+        std::ofstream file(path, std::ios::binary);
+
+        Core::DbHeader header{};
+        header.version = 0.1;
+
+        file.write((char *)&header, sizeof(header));
+        file.close();
+    }
+
+    void WorkFile::read_file(const char *path)
+    {
+        Core::DbHeader header;
+        std::ifstream file(path, std::ios::binary);
+        file.read(reinterpret_cast<char *>(&header), sizeof(header));
+
+        if (memcmp(header.magic, "HELLNAH", 8) != 0)
+        {
+            throw std::runtime_error("The file is not a HELLNAH database!");
+        }
+    }
+
+    Core::WorkFile::WorkFile(const char *path)
+    {
+        if (!is_exists_file(path))
+        {
+            create_file(path);
+        }
+        else
+        {
+            read_file(path);
+        }
+
+        strcpy(this->path, path);
+    }
 }
